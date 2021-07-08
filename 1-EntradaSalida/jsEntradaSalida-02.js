376b040dbce379feaf5a8d199b38aed9e8e19f14
AgustinFernandez73 <j11920agus@gmail.com>
2021-07-07 22:04:49 -0300

    ejercicios segunda clase

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,12 +1,11 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
+function mostrar() {
 	// Declaración de una variable
 	let nombre;
 
-	nombre = prompt("Ingrese su nombre");
+	nombre = prompt("Ingrese su nombre: ");
 
 	alert(nombre);
 }
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -2,24 +2,65 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
-function sumar()
-{	
-	alert("ok");	
+ej.: "la Resta es 750"*/
+function sumar() {
+
+	// Declaración de variables
+	let numero1;
+	let numero2;
+	let resultado;
+
+	// Relevos los datos de entrada
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	// Realizo las operaciones
+	resultado = numero1 + numero2;
+
+	// Muestro los resultados
+	alert("El resultado es " + resultado);
 }
 
-function restar()
-{
-	
+function restar() {
+	let numero1;
+	let numero2;
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numero1 - numero2;
+
+	alert("El resultado es " + resultado);
 }
 
-function multiplicar()
-{ 
-	
+function multiplicar() {
+	let numero1;
+	let numero2;
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = numero1 * numero2;
+
+	alert("El resultado es " + resultado);
 }
 
-function dividir()
-{
-	
+function dividir() {
+	let numero1;
+	let numero2;
+	let resultado;
+
+	numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	if (numero2 === 0) {
+		alert("No se puede dividir por 0");
+	}
+	else {
+		resultado = numero1 / numero2;
+		alert("El resultado es " + resultado);
+	}
 }
 
